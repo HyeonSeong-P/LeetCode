2a8c0c9f173c62044e920b631629b36697316e1f
HyeonSeong-P <62276074+HyeonSeong-P@users.noreply.github.com>
2023-03-15 17:32:08 +0900

    Time: 155 ms (100.00%), Space: 35.3 MB (75.00%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.kt
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.kt
@@ -10,32 +10,30 @@
  */
 class Solution {
     fun isCompleteTree(root: TreeNode?): Boolean {
-        val queue: Queue<Pair<TreeNode?, Int>> = LinkedList()
+        val queue: Queue<TreeNode?> = LinkedList()
         var shouldLeafNode = false
-        queue.add(root to 0)
+        queue.add(root)
         while(queue.isNotEmpty()) {
-            val curr = queue.poll()
-            val currNode = curr.first ?: continue
-            val currHeight = curr.second 
+            val currNode = queue.poll() ?: continue
             when {
                 currNode.left != null && currNode.right != null -> {
                     if(shouldLeafNode) return false
-                    queue.add(currNode.left to currHeight + 1)
-                    queue.add(currNode.right to currHeight + 1)
+                    queue.add(currNode.left)
+                    queue.add(currNode.right)
                 }
                 currNode.left == null && currNode.right != null -> {
                     return false
                 }
                 currNode.left == null && currNode.right == null -> {
                     shouldLeafNode = true
-                    queue.add(currNode.left to currHeight + 1)
-                    queue.add(currNode.right to currHeight + 1)
+                    queue.add(currNode.left)
+                    queue.add(currNode.right)
                 }
                 else -> {
                     if(shouldLeafNode) return false
                     shouldLeafNode = true
-                    queue.add(currNode.left to currHeight + 1)
-                    queue.add(currNode.right to currHeight + 1)
+                    queue.add(currNode.left)
+                    queue.add(currNode.right)
                 }
             }
         }    
